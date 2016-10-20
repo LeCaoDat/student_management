4ae03a9aebe3cae051307a9e75474994c3544c60
LeCaoDat <hoangtuut1909@gmail.com>
2016-10-20 22:40:17 +0700

    Upload Exercise Student Management

--- src/app/MainContent/MainContent.ts
+++ src/app/MainContent/MainContent.ts
@@ -1,6 +1,4 @@
 import {Component} from '@angular/core';
-import {Teacher} from '../Teacher/Teacher';
-import {Student} from '../Student/Student';
 import { Angular2DataTableModule } from 'angular2-data-table';
 import {
   TableOptions,
@@ -10,14 +8,13 @@ import {
 
 @Component({
   selector: 'main-content',
+  inputs: ['rows'],
   templateUrl: './MainContent.html',
   styleUrls: ['./MainContent.css'],
 
 })
 export class MainContent {
 
-  teacher: Teacher;
-  viewState: number;
   rows = [];
 
   options = new TableOptions({
@@ -35,24 +32,5 @@ export class MainContent {
       new TableColumn({ name: 'Status', sortable: true }),
     ]
   });
-
-  constructor() {
-    this.teacher = new Teacher('Uchiha', 'Obito', '19/09/1996');
-    // this.teacher.addStudent('Lê Cao','Đạt','19/09/1996',8.0,8.0);
-    // this.teacher.addStudent('Võ Thị','A','09/03/1996',8.0,8.0);
-    this.fetch((Student) => {
-      this.teacher.students.push(...Student);
-    });
-    this.rows = this.teacher.students;
-  }
-
-  fetch(cb) {
-    const req = new XMLHttpRequest();
-    req.open('GET', '../../resources/data.json');
-    req.onload = () => {
-      cb(JSON.parse(req.response));
-    };
-    req.send();
-  }
 
 }
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,28 @@
 import {Component} from '@angular/core';
+import {Teacher} from './Teacher/Teacher';
+import {Student} from './Student/Student';
 
 @Component({
   selector   : 'app',
   templateUrl: './app.html',
 })
 export class AppComponent {
+  teacher: Teacher;
+  constructor() {
+    this.teacher = new Teacher('Uchiha', 'Obito', '19/09/1996');
+    // this.teacher.addStudent('Lê Cao','Đạt','19/09/1996',8.0,8.0);
+    // this.teacher.addStudent('Võ Thị','A','09/03/1996',8.0,8.0);
+    this.fetch((Student) => {
+      this.teacher.students.push(...Student);
+    });
+  }
+
+  fetch(cb) {
+    const req = new XMLHttpRequest();
+    req.open('GET', './resources/data.json');
+    req.onload = () => {
+      cb(JSON.parse(req.response));
+    };
+    req.send();
+  }
 }
